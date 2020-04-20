8f96ee06464210c47f233c1042678aad4e8ad927
joseph704 <freelove54@naver.com>
2020-04-20 18:50:26 +0900

    데이터 생성부 함수 생성

--- iNaViSystems/main.c
+++ iNaViSystems/main.c
@@ -7,9 +7,41 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+FILE *fp;
+
+void create_data(int max_count,char *fileName,int *data){
+    fp = fopen(fileName, "w");
+    int i, sub_i;
+    srand((unsigned int)time(NULL));
+    for (i = 0;i<max_count;i++){
+        data[i] = rand() % 1000000 + 1;
+        for (sub_i = 0; sub_i<i;sub_i++){
+            if (data[i] == data[sub_i]) {
+                i--;
+                break;
+            }
+        }
+    }
+    for (i = 0; i < max_count; i++) fprintf(fp,"%d\n", data[i]);
+    fclose(fp);
+}
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a_count = 20;
+    int b_count = 100;
+    int data_a[a_count];
+    int data_b[b_count];
+    int i,j;
+    create_data(a_count, "a.txt",&data_a);
+    usleep(100000);
+    create_data(b_count, "b.txt",&data_b);
+    
     return 0;
 }
+
+
