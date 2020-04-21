bb5408df80b3f064125d5a0825b210ee58107e5a
joseph704 <freelove54@naver.com>
2020-04-21 23:40:36 +0900

    교집합 구하는 함수 작성!

--- iNaViSystems/main.c
+++ iNaViSystems/main.c
@@ -32,16 +32,23 @@ void create_data(int max_count,char *fileName,int *data){
 
 
 int main(int argc, const char * argv[]) {
-    int a_count = 20;
-    int b_count = 100;
-    int data_a[a_count];
-    int data_b[b_count];
+    int a_count = 100000;
+    int b_count = 1000;
+    int data_a[a_count],data_b[b_count];
     int i,j;
-    create_data(a_count, "a.txt",&data_a);
-    usleep(100000);
-    create_data(b_count, "b.txt",&data_b);
     
+    create_data(a_count, "a.txt",data_a);
+    usleep(100000);
+    create_data(b_count, "b.txt",data_b);
+    fp = fopen("r.txt", "w");
+    for (i=0;i<a_count;i++){
+        for (j=0;j<b_count;j++){
+            if(data_a[i]==data_b[j]){
+                fprintf(fp,"%d\n", data_a[i]);
+            }
+        }
+    }
+    printf("Finished");
+    fclose(fp);
     return 0;
 }
-
-
