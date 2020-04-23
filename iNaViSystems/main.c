74a6820f0b27494c54e22aec9ccad6a81d2c8d4e
joseph704 <freelove54@naver.com>
2020-04-23 16:35:41 +0900

    주석첨가

--- iNaViSystems/main.c
+++ iNaViSystems/main.c
@@ -17,15 +17,15 @@ void create_data(int max_count,char *fileName,int *data){
     int i, sub_i;
     srand((unsigned int)time(NULL));
     for (i = 0;i<max_count;i++){
-        data[i] = rand() % 1000000 + 1;
+        data[i] = rand() % 1000000 + 1;     // 1~1000000까지 난수 발생
         for (sub_i = 0; sub_i<i;sub_i++){
-            if (data[i] == data[sub_i]) {
+            if (data[i] == data[sub_i]) {     // 중복 배제
                 i--;
                 break;
             }
         }
     }
-    for (i = 0; i < max_count; i++) fprintf(fp,"%d\n", data[i]);
+    for (i = 0; i < max_count; i++) fprintf(fp,"%d\n", data[i]); // a.txt 또는 b.txt 에 난수들 저장
     fclose(fp);
 }
 
@@ -37,18 +37,18 @@ int main(int argc, const char * argv[]) {
     int data_a[a_count],data_b[b_count];
     int i,j;
     
-    create_data(a_count, "a.txt",data_a);
+    create_data(a_count, "a.txt",data_a);    // data_a : a.txt 난수들의 배열
     usleep(100000);
-    create_data(b_count, "b.txt",data_b);
+    create_data(b_count, "b.txt",data_b);    // data_b : b.txt 난수들의 배열
     fp = fopen("r.txt", "w");
     for (i=0;i<a_count;i++){
         for (j=0;j<b_count;j++){
             if(data_a[i]==data_b[j]){
-                fprintf(fp,"%d\n", data_a[i]);
+                fprintf(fp,"%d\n", data_a[i]); // a.txt 난수들과 b.txt 난수들의 교집합 r.txt에 저장
             }
         }
     }
-    printf("Finished");
+    printf("Finished\n");
     fclose(fp);
     return 0;
 }
